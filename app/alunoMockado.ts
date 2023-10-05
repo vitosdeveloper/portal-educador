db0e1ed7361d5d411ad41e33eaeecf48f68e47cc
vitosnatios <vitosdeveloper@gmail.com>
2023-10-05 17:04:42 -0300

    alunos das turmas e as notas dos bimestres, transformar em table depois

--- app/alunoMockado.ts
+++ app/alunoMockado.ts
@@ -3,7 +3,7 @@ export const alunosMockados = [
     nome: 'Leozin Lipe Parklez',
     idade: 22,
     turma: '1-m',
-    bimestres: [
+    materias: [
       [
         {
           materia: 'Língua Portuguesa',
@@ -11,7 +11,20 @@ export const alunosMockados = [
           bimestres: [
             { teste: 10, prova: 10, presenca: 4, tarefas: 4, comportamento: 2 },
             { teste: 8, prova: 6, presenca: 4, tarefas: 3, comportamento: 2 },
-            { teste: 7, prova: 7, presenca: 2, tarefas: 4, comportamento: 2 },
+            {
+              teste: 7,
+              prova: 7,
+              presenca: 2,
+              tarefas: 4,
+              comportamento: 2,
+            },
+            {
+              teste: null,
+              prova: null,
+              presenca: null,
+              tarefas: 2,
+              comportamento: null,
+            },
           ],
         },
         {
@@ -20,10 +33,58 @@ export const alunosMockados = [
             { teste: 10, prova: 10, presenca: 4, tarefas: 4, comportamento: 2 },
             { teste: 8, prova: 6, presenca: 4, tarefas: 3, comportamento: 2 },
             { teste: 7, prova: 7, presenca: 2, tarefas: 4, comportamento: 2 },
+            {
+              teste: null,
+              prova: null,
+              presenca: null,
+              tarefas: 3,
+              comportamento: null,
+            },
           ],
         },
       ],
     ],
     matriculado: true,
+  },
+  {
+    nome: 'Iago Orkon Pericles',
+    idade: 17,
+    turma: '1-m',
+    materias: [
+      [
+        {
+          materia: 'Língua Portuguesa',
+          //dividir cada bimestre por 3 pra tirar a média total do bimestre
+          bimestres: [
+            { teste: 8, prova: 6, presenca: 3, tarefas: 3, comportamento: 1 },
+            { teste: 10, prova: 6, presenca: 4, tarefas: 3, comportamento: 2 },
+            { teste: 5, prova: 7, presenca: 2, tarefas: 4, comportamento: 2 },
+            {
+              teste: null,
+              prova: null,
+              presenca: null,
+              tarefas: 3,
+              comportamento: null,
+            },
+          ],
+        },
+        {
+          materia: 'Física',
+          bimestres: [
+            { teste: 10, prova: 10, presenca: 4, tarefas: 4, comportamento: 2 },
+            { teste: 8, prova: 6, presenca: 4, tarefas: 3, comportamento: 2 },
+            { teste: 7, prova: 7, presenca: 2, tarefas: 4, comportamento: 2 },
+            {
+              teste: null,
+              prova: null,
+              presenca: null,
+              tarefas: 3,
+              comportamento: null,
+            },
+          ],
+        },
+      ],
+    ],
+    matriculado: false,
   },
 ];
--- app/turmas/[turma]/[materia]/page.tsx
+++ app/turmas/[turma]/[materia]/page.tsx
@@ -1,3 +1,4 @@
+import { alunosMockados } from '@/app/alunoMockado';
 import ErrorPage from '@/app/components/ErrorPage';
 import Title from '@/app/components/Text/Title';
 import Header from '@/app/components/partials/Header';
@@ -14,20 +15,93 @@ import React from 'react';
 
 type Props = { params: { turma: string; materia: string } };
 
+//apenas organizando os dados para fazer uma bela table depois
 const MateriaPage = ({ params: { turma, materia } }: Props) => {
   try {
     const turmaObj = turmas.find((t) => t.slug === turma)!;
+    if (!turmaObj) er('Essa turma ' + turma + ' não existe.');
     const materiaObj = materias.find((m) => m.slug === materia)!;
-    if (!turmaObj) return er('Essa turma ' + turma + ' não existe.');
+    if (!materiaObj) er('Essa matéria ' + materia + ' não existe.');
     if (!doesProfessorTeachHere(professor, turmaObj))
       er('Você não dá aula na ' + turmaObj.turma + '.');
     if (!doesProfessorTeachThis(professor, materiaObj.materia))
       er('Você não dá aula de ' + materiaObj.materia + '.');
+    //lembrar de trazer os alunos em ordem alfabética
+    const alunosDaTurma = alunosMockados.filter(
+      (alunoMock) => alunoMock.matriculado && alunoMock.turma === turma
+    );
+    //pode até preencher 4 bimestres com o quarto estando vazio ou com - nos null,
+    //mas só conte a média considerando os 3, por isso essa constante
+    let quantidadeDeBimestres = 0;
+    const [bimestresDeAlgumaMateria] = alunosDaTurma[0].materias[0];
+    for (let i = 0; i < bimestresDeAlgumaMateria.bimestres.length; i++) {
+      const { comportamento, presenca, prova, teste, tarefas } =
+        bimestresDeAlgumaMateria.bimestres[i];
+      if (
+        !Number.isInteger(comportamento) ||
+        !Number.isInteger(presenca) ||
+        !Number.isInteger(prova) ||
+        !Number.isInteger(teste) ||
+        !Number.isInteger(tarefas)
+      ) {
+        quantidadeDeBimestres = i;
+        break;
+      }
+    }
+
+    // console.log(quantidadeDeBimestres);
+
     return (
       <Box sx={{ textAlign: 'center' }}>
         <Header />
         <Title>{turmaObj.turma}</Title>
         <h3>{materiaObj.materia}</h3>
+        {alunosDaTurma.map((alunoDaTurma) => (
+          <div key={alunoDaTurma.nome}>
+            <p>
+              {alunoDaTurma.nome} - {alunoDaTurma.idade} anos
+            </p>
+            {alunoDaTurma.materias.map((mate, index) => {
+              return mate
+                .filter((m) => m.materia === materiaObj.materia)
+                .map(({ materia, bimestres }) => {
+                  return (
+                    <div key={materia}>
+                      <p>{materia}</p>
+                      {bimestres.map((bimestre, i) => {
+                        const {
+                          comportamento,
+                          presenca,
+                          prova,
+                          teste,
+                          tarefas,
+                        } = bimestre;
+                        const media = (
+                          (Number(comportamento) +
+                            Number(presenca) +
+                            Number(prova) +
+                            Number(teste) +
+                            Number(tarefas)) /
+                          3
+                        ).toFixed(1);
+                        return (
+                          <div key={Math.random()}>
+                            <h3>Bimestre: {i + 1}</h3>
+                            <p>Comportamento: {comportamento}</p>
+                            <p>Presença: {presenca}</p>
+                            <p>Prova: {prova}</p>
+                            <p>Teste: {teste}</p>
+                            <p>Tarefas: {tarefas}</p>
+                            <h4>Média: {media}</h4>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  );
+                });
+            })}
+          </div>
+        ))}
       </Box>
     );
   } catch (error) {
